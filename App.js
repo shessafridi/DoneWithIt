c805737dd0560406d704b0e7f1bba6cee92b0edd
shessafridi <mshesskhan@gmail.com>
2021-09-06 21:51:50 +0500

    Implemented the picker component

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, View } from 'react-native';
 
 import WelcomeScreen from './app/screens/WelcomeScreen';
@@ -9,32 +9,38 @@ import colors from './app/config/colors';
 import MessagesScreen from './app/screens/MessagesScreen';
 import AccountScreen from './app/screens/AccountScreen';
 import ListingsScreen from './app/screens/ListingsScreen';
+import AppPicker from './app/components/AppPicker';
+import AppTextInput from './app/components/AppTextInput';
+import Screen from './app/components/Screen';
+
+const categories = [
+  {
+    label: 'Furniture',
+    value: 1,
+  },
+  {
+    label: 'Clothing',
+    value: 2,
+  },
+  {
+    label: 'Cameras',
+    value: 3,
+  },
+];
 
 export default function App() {
+  const [category, setCategory] = useState(categories[0]);
   return (
-    // <View
-    //   style={{
-    //     padding: 20,
-    //     height: '100%',
-    //     backgroundColor: colors.background,
-    //   }}
-    // >
-    //   <Card
-    //     title='Testing my card'
-    //     subTitle='100$'
-    //     image={require('./app/assets/jacket.jpg')}
-    //   />
-    //   <Card
-    //     title='Luxary Couch For Sale!'
-    //     subTitle='900$'
-    //     image={require('./app/assets/couch.jpg')}
-    //   />
-    // </View>
-
-    // <WelcomeScreen />
-    // <MessagesScreen />
-    // <AccountScreen />
-    <ListingsScreen />
+    <Screen>
+      <AppPicker
+        items={categories}
+        selectedItem={category}
+        onSelectItem={category => setCategory(category)}
+        icon='apps'
+        placeholder='Category'
+      />
+      <AppTextInput icon='email' />
+    </Screen>
   );
 }
 
